03be224ad8b5b79d33e5573c003a5b659dc1959e
RonSheininTau <64597329+RonSheininTau@users.noreply.github.com>
2022-04-23 17:49:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
 import os
-#os.environ["R_HOME"] = r"C:\Program Files\R\R-4.1.1"
-#os.environ['path'] += r";C:\Program Files\R\R-4.1.1\bin"
 from rpy2.robjects import r
 from rpy2 import robjects as ro
 from rpy2.robjects.conversion import localconverter
