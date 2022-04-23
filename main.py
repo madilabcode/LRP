720294782ee7c8c96a5598dd34358dd8aaaebc16
RonSheininTau <64597329+RonSheininTau@users.noreply.github.com>
2022-04-23 17:31:19 +0300

    V 23.4

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 import os
-os.environ["R_HOME"] = r"C:\Program Files\R\R-4.1.1"
-os.environ['path'] += r";C:\Program Files\R\R-4.1.1\bin"
+#os.environ["R_HOME"] = r"C:\Program Files\R\R-4.1.1"
+#os.environ['path'] += r";C:\Program Files\R\R-4.1.1\bin"
 from rpy2.robjects import r
 from rpy2 import robjects as ro
 from rpy2.robjects.conversion import localconverter
@@ -13,7 +13,6 @@ from Codes import neo_connection_db as neo
 from Codes import utils
 import rpy2.robjects.pandas2ri as rpyp
 import concurrent.futures
-import pickle
 import warnings
 
 pd.options.mode.chained_assignment = None  # default='warn'
@@ -122,10 +121,6 @@ def _helper_LRP(x):
         return LRP(x[0], x[1], x[2], plot_path=x[3])
 
 
-def _helper_Classfier(args):
-    return build_or_use_classfier(*args)
-
-
 def ExpTables(obj, toName, fromName, assay=assay):
     with localconverter(default_converter + rpyp.converter):
         r("library(Seurat)")
@@ -139,7 +134,7 @@ def ExpTables(obj, toName, fromName, assay=assay):
         return toExpression, fromExpression
 
 
-def run_pipline(args, objName, max_workers=1):
+def run_pipline(args, objName, max_workers=3):
     r["source"]("Codes/Ligand_Receptor_pipeline.R")
     legRetList = {}
     DSA_Tables = {}
@@ -172,212 +167,28 @@ def run_pipline(args, objName, max_workers=1):
         utils.save_obj(DSA_Graphs, f"./outputObj/DSA_Graphs_{objName}")
         utils.save_obj(DSA_Mean, f"./outputObj/DSA_mean_{objName}")
 
-    # print([legRetList, DSA_Tables, DSA_Graphs, DSA_Mean])
-    # del result
     r("rm(list = ls())")
     r("gc()")
     return legRetList, DSA_Tables, DSA_Graphs, DSA_Mean
 
 
-def deCirces(obj, toName, fromName, legRet, DSA_Table, DSA_Graph, DE_Recp, path, threshold=0.1):
-    r["source"]("Codes/Ligand_Receptor_pipeline.R")
-    findMarkers_python = r("findMarkers_python")
-    DElegenedNcolor = r("DElegenedNcolor")
-    DSAlegenedNcolor = r("DSAlegenedNcolor")
-    createCircosPlots = r("createCircosPlots")
-
-    with localconverter(default_converter + rpyp.converter):
-        markerallL = findMarkers_python(obj, fromName, toName, genes_to_use=list(legRet["Ligand"]), threshold=threshold)
-        markerallR = findMarkers_python(obj, toName, fromName, genes_to_use=list(legRet["Receptor"]),
-                                        threshold=threshold)
-
-        markerallL["isL"] = list(map(lambda x: x in list(legRet["Ligand"]), markerallL.index))
-        markerallR["isR"] = list(map(lambda x: x in list(legRet["Receptor"]), markerallR.index))
-
-        markerallL = markerallL.loc[markerallL["isL"], :]
-        markerallR = markerallR.loc[markerallR["isR"], :]
-
-        if len(markerallL.index) > 0 and len(markerallR.index) > 0:
-            DE = DElegenedNcolor(obj, fromName, toName, legRet, markerallL, markerallR)
-            DSA = DSAlegenedNcolor(DSA_Table)
-            toExpression, fromExpression = ExpTables(obj, toName, fromName)
-
-            createCircosPlots(toExpression, legRet, DE, ro.ListVector(DSA_Graph.Tfs_Per_Recp_in_max_flow()), DSA,
-                              fromName, toName, "de", obj,
-                              DE_Recp, path=path)
-
-
-def RUN_DE_LR(conf, lst_Tr, lst_Co, pathTr, pathCo, max_workers=1):
-    r["source"]("Codes/Ligand_Receptor_pipeline.R")
-    toVec = list(conf.loc[conf["Var"] == "toVecForDE", "Value"])[0].split()
-    fromVec = list(conf.loc[conf["Var"] == "fromVecForDE", "Value"])[0].split()
-
-    objTr = r(f"objTr = readRDS('{pathTr}')")
-    objCo = r(f"objCo = readRDS('{pathCo}')")
-
-    toVec = utils.intersection(toVec, lst_Tr[0].keys())
-    toVec = utils.intersection(toVec, lst_Co[0].keys())
-
-    name_obj_clf = list(conf.loc[conf["Var"] == "nameObjCLF", "Value"])[0]
-    plotpathT = list(conf.loc[conf["Var"] == "DEplotpathT", "Value"])[0]
-    plotpathC = list(conf.loc[conf["Var"] == "DEplotpathC", "Value"])[0]
-
-    args = []
-    for toName in toVec:
-        fromVecTemp = utils.intersection(fromVec, lst_Tr[0][toName].keys())
-        fromVecTemp = utils.intersection(fromVecTemp, lst_Co[0][toName].keys())
-        for fromName in fromVecTemp:
-            if toName != fromName:
-                toExpressionTr, fromExpressionTr = ExpTables(objTr, toName, fromName)
-                toExpressionCo, fromExpressionCo = ExpTables(objCo, toName, fromName)
-                args.append((toExpressionTr, toExpressionCo, fromExpressionTr, fromExpressionCo,
-                             f"plotOut/ROC/RocResult{fromName}_{toName}", lst_Tr[0][toName][fromName],
-                             lst_Co[0][toName][fromName]))
-
-    # with BoundedProcessPoolExecutor(max_workers=max_workers) as executor:
-    with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
-
-        results = list(executor.map(_helper_Classfier, args))
-
-    counter = 0
-    up = {}
-    down = {}
-
-    for toName in toVec:
-        up[toName] = {}
-        down[toName] = {}
-        for fromName in fromVec:
-            if toName != fromName and len(results) > counter:
-                if len(results[counter]) > 1:
-                    up[toName][fromName] = pd.DataFrame(results[counter][1])
-                    up[toName][fromName]["importances value"] = up[toName][fromName]["importances value"] / \
-                                                                up[toName][fromName]["importances value"].max()
-                    up[toName][fromName]["importances value"] = up[toName][fromName]["importances value"].apply(
-                        lambda x: x if x > 0.3 else 0.3)
-
-                if len(results[counter]) > 2:
-                    down[toName][fromName] = pd.DataFrame(results[counter][2])
-                    down[toName][fromName]["importances value"] = down[toName][fromName]["importances value"] / \
-                                                                  down[toName][fromName]["importances value"].max()
-                    down[toName][fromName]["importances value"] = down[toName][fromName]["importances value"].apply(
-                        lambda x: x if x > 0.3 else 0.3)
-            counter += 1
-
-    utils.save_obj(up, f"outputObj/up_{name_obj_clf}")
-    utils.save_obj(down, f"outputObj/down_{name_obj_clf}")
-
-    # with BoundedProcessPoolExecutor(max_workers=max_workers) as executor:
-    # with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
-
-    for toName in toVec:
-        fromVecTemp = utils.intersection(fromVec, down[toName].keys())
-        fromVecTemp = utils.intersection(fromVecTemp, up[toName].keys())
-        for fromName in fromVecTemp:
-            if fromName != toName:
-                if len(down[toName][fromName].index) > 0:
-                    deCirces(objCo, toName, fromName, lst_Co[0][toName][fromName],
-                             lst_Co[1][toName][fromName],
-                             lst_Co[2].return_GD_from_key(toName).return_GD_from_key(fromName),
-                             list(down[toName][fromName]["feture"]), plotpathC)
-
-                if len(up[toName][fromName].index) > 0:
-                    # executor.submit
-                    deCirces(objTr, toName, fromName, lst_Tr[0][toName][fromName],
-                             lst_Tr[1][toName][fromName],
-                             lst_Tr[2].return_GD_from_key(toName).return_GD_from_key(fromName),
-                             list(up[toName][fromName]["feture"]), plotpathT)
-
-    return up, down
-
-
-def build_or_use_classfier(toExp1, toExp2, fromExp1, fromExp2, RocPlotName,
-                           lr1, lr2=None, modle=None):
-    r["source"]("Codes/Ligand_Receptor_pipeline.R")
-    ProtNamesInfo = pd.read_csv("files/humanProtinInfo.csv")
-    ProtActions = pd.read_csv("files/humanProtinAction.csv")
-    lst = pyd.main_py_to_R(ProtNamesInfo, ProtActions)
-    if lr2 is not None:
-        legRet = pd.concat([lr1, lr2])
-    else:
-        legRet = lr1
-    with localconverter(default_converter + rpyp.converter):
-        DSA_Table_1 = tfg.DSA_anaylsis(toExp1, legRet["Receptor"], ProtActions, ProtNamesInfo, tfs=lst[0])
-        DSA_Table_2 = tfg.DSA_anaylsis(toExp2, legRet["Receptor"], ProtActions, ProtNamesInfo, tfs=lst[0])
-
-    args1 = [toExp1, fromExp1, DSA_Table_1[0], legRet]
-    args2 = [toExp2, fromExp2, DSA_Table_2[0], legRet]
-
-    if modle is None:
-        return utils.DSA_Classfier(args1, args2, return_modle=False, plot_name=RocPlotName)
-    return utils.DSA_Classfier(args1, args2, modle=modle, return_modle=False, plot_name=RocPlotName)
-
-
 def main():
     try:
-        conf = pd.read_csv("config.csv")
+        conf = pd.read_csv(r"./config.csv")
     except:
         raise Exception("no input CSV found")
 
-    pathTret = list(conf.loc[conf["Var"] == "objTr", "Value"])[0]
-    pathControl = list(conf.loc[conf["Var"] == "objCo", "Value"])[0]
-    #utils.normalize_scale_exp(pathTret,pathControl)
-    if type(pathControl) == float and np.isnan(pathControl):
-        flag = False
-    else:
-        flag = True
-
-    plotpath = list(conf.loc[conf["Var"] == "plotpathTr", "Value"])[0]
+    path = list(conf.loc[conf["Var"] == "obj_name", "Value"])[0]
+    plotpath = list(conf.loc[conf["Var"] == "Plotpath", "Value"])[0]
     toVec = list(conf.loc[conf["Var"] == "toVec", "Value"])[0].split(" ")
     fromVec = list(conf.loc[conf["Var"] == "fromVec", "Value"])[0].split(" ")
-    args = [(pathTret, toName, fromName, plotpath) for toName in toVec for fromName in fromVec if
+    args = [(path, toName, fromName, plotpath) for toName in toVec for fromName in fromVec if
             fromName != toName]
 
-    name_obj_Tr = list(conf.loc[conf["Var"] == "nameObjTr", "Value"])[0]
-    lst_Tr = run_pipline(args, name_obj_Tr)
-    if flag:
-        plotpath = list(conf.loc[conf["Var"] == "plotpathCo", "Value"])[0]
-        args = [(pathControl, toName, fromName, plotpath) for toName in toVec for fromName in fromVec if
-                fromName != toName]
-
-        name_obj_Co = list(conf.loc[conf["Var"] == "nameObjCo", "Value"])[0]
-        lst_Co = run_pipline(args, name_obj_Co)
-        up, down = RUN_DE_LR(conf, lst_Tr, lst_Co, f"InputObj/{pathTret}", f"InputObj/{pathControl}")
-
-        projName = list(conf.loc[conf["Var"] == "Proj", "Value"])[0]
-        graph = neo.start_connection()
-        neo.delete_nodes_by_prop(graph, prop="project", key=projName, delete_all_grpah=False)
-
-        for toName in toVec:
-            toNodeTr = neo.add_node_to_neo(graph, toName, name_obj_Tr, projName)
-            toNodeCo = neo.add_node_to_neo(graph, toName, name_obj_Co, projName)
-
-            neo.crate_all_from_nodes(graph, toNodeTr, lst_Tr[3][toName],
-                                     lst_Tr[1][toName], lst_Tr[0][toName], lst_Tr[2].return_GD_from_key(toName),
-                                     name_obj_Tr, projName)
-
-            neo.crate_all_from_nodes(graph, toNodeCo, lst_Co[3][toName],
-                                     lst_Co[1][toName], lst_Co[0][toName], lst_Co[2].return_GD_from_key(toName),
-                                     name_obj_Co, projName)
-
-            neo.make_cluster_trat_relationship(graph, toNodeTr, toNodeCo, down[toName])
-
-            neo.make_cluster_trat_relationship(graph, toNodeCo, toNodeTr, up[toName])
-
-        return lst_Tr, lst_Co
-
-    else:
-
-        projName = list(conf.loc[conf["Var"] == "Proj", "Value"])[0]
-        graph = neo.start_connection()
-        neo.delete_nodes_by_prop(graph, prop="project", key=projName, delete_all_grpah=False)
-
-        for toName in toVec:
-            toNodeTr = neo.add_node_to_neo(graph, toName, name_obj_Tr, projName)
-            neo.crate_all_from_nodes(graph, toNodeTr, lst_Tr[3][toName],
-                                     lst_Tr[1][toName], lst_Tr[0][toName], lst_Tr[2].return_GD_from_key(toName),
-                                     name_obj_Tr, projName)
-
-    return lst_Tr
+    obj_name = list(conf.loc[conf["Var"] == "Project_name", "Value"])[0]
+    lst_obj = run_pipline(args, obj_name)
+    
+    return lst_obj
 
 
 def format_dsa_file(x):
@@ -392,157 +203,6 @@ def format_dsa_file(x):
         return f"{x}/{type_cluster}"
     return f"{x}/{type_cluster}/{subtype_cluster}"
 
-
-def DE_DSA():
-    conf = pd.read_csv("config.csv")
-
-    name_obj_Tr = list(conf.loc[conf["Var"] == "nameObjTr", "Value"])[0]
-    name_obj_Co = list(conf.loc[conf["Var"] == "nameObjCo", "Value"])[0]
-
-    DSA_MeanTr = tfg.load_obj(f"outputObj/DSA_mean_{name_obj_Tr}")
-    DSA_MeanCo = tfg.load_obj(f"outputObj/DSA_mean_{name_obj_Co}")
-
-    DSA_UP = pd.DataFrame()
-    DSA_DOWN = pd.DataFrame()
-
-    for key, value in DSA_MeanTr.items():
-        value["To Cluster"] = key
-        value = value[["Cluster", "To Cluster", "DSA_Mean"]]
-        value.columns = (["From Cluster", "To Cluster", "DSA_Mean"])
-        DSA_UP = pd.concat([DSA_UP, value])
-
-    for key, value in DSA_MeanCo.items():
-        value["To Cluster"] = key
-        value = value[["Cluster", "To Cluster", "DSA_Mean"]]
-        value.columns = (["From Cluster", "To Cluster", "DSA_Mean"])
-        DSA_DOWN = pd.concat([DSA_DOWN, value])
-
-    DSA_UP["From Cluster"] = DSA_UP["From Cluster"].apply(format_dsa_file)
-    DSA_UP["To Cluster"] = DSA_UP["To Cluster"].apply(format_dsa_file)
-
-    DSA_DOWN["From Cluster"] = DSA_DOWN["From Cluster"].apply(format_dsa_file)
-    DSA_DOWN["To Cluster"] = DSA_DOWN["To Cluster"].apply(format_dsa_file)
-
-    DSA_DOWN.to_csv(r"./files/DSA_DOWN.csv")
-    DSA_UP.to_csv(r"./files/DSA_UP.csv")
-
-    return DSA_UP, DSA_DOWN
-
-
-def test_DE_Intercations():
-    r["source"]("Codes/Ligand_Receptor_pipeline.R")
-    conf = pd.read_csv(("config.csv"))
-    pathTret = list(conf.loc[conf["Var"] == "objTr", "Value"])[0]
-    pathControl = list(conf.loc[conf["Var"] == "objCo", "Value"])[0]
-    objTr = r(f"objTr = readRDS('InputObj/{pathTret}')")
-    objCo = r(f"objCo = readRDS('InputObj/{pathControl}')")
-    toVec = list(conf.loc[conf["Var"] == "toVec", "Value"])[0].split(" ")
-    fromVec = list(conf.loc[conf["Var"] == "fromVec", "Value"])[0].split(" ")
-    name_obj_Tr = list(conf.loc[conf["Var"] == "nameObjTr", "Value"])[0]
-    name_obj_Co = list(conf.loc[conf["Var"] == "nameObjCo", "Value"])[0]
-    name_obj_clf = list(conf.loc[conf["Var"] == "nameObjCLF", "Value"])[0]
-    plotpathT = list(conf.loc[conf["Var"] == "DEplotpathT", "Value"])[0]
-    plotpathC = list(conf.loc[conf["Var"] == "DEplotpathC", "Value"])[0]
-
-    legRetListTr = tfg.load_obj(f"outputObj/legRetLists_{name_obj_Tr}")
-    DSA_TablesTr = tfg.load_obj(f"outputObj/DSA_Tables_{name_obj_Tr}")
-    DSA_GraphsTr = tfg.load_obj(f"outputObj/DSA_Graphs_{name_obj_Tr}")
-    DSA_MeanTr = tfg.load_obj(f"outputObj/DSA_mean_{name_obj_Tr}")
-    lst_Tr = (legRetListTr, DSA_TablesTr, DSA_GraphsTr, DSA_MeanTr)
-
-    legRetListCo = tfg.load_obj(f"outputObj/legRetLists_{name_obj_Co}")
-    DSA_TablesCo = tfg.load_obj(f"outputObj/DSA_Tables_{name_obj_Co}")
-    DSA_GraphsCo = tfg.load_obj(f"outputObj/DSA_Graphs_{name_obj_Co}")
-    DSA_MeanCo = tfg.load_obj(f"outputObj/DSA_mean_{name_obj_Co}")
-    lst_Co = (legRetListCo, DSA_TablesCo, DSA_GraphsCo, DSA_MeanCo)
-
-    up = tfg.load_obj(f"outputObj/up_{name_obj_clf}")
-    down = tfg.load_obj(f"outputObj/up_{name_obj_clf}")
-    up, down = RUN_DE_LR(conf, lst_Tr, lst_Co, f"InputObj/{pathTret}", f"InputObj/{pathControl}")
-
-
-def test_function():
-    r["source"]("Codes/Ligand_Receptor_pipeline.R")
-    conf = pd.read_csv(("config.csv"))
-    pathTret = list(conf.loc[conf["Var"] == "objTr", "Value"])[0]
-    pathControl = list(conf.loc[conf["Var"] == "objCo", "Value"])[0]
-    objTr = r(f"objTr = readRDS('InputObj/{pathTret}')")
-    objCo = r(f"objCo = readRDS('InputObj/{pathControl}')")
-    toVec = list(conf.loc[conf["Var"] == "toVec", "Value"])[0].split(" ")
-    fromVec = list(conf.loc[conf["Var"] == "fromVec", "Value"])[0].split(" ")
-    name_obj_Tr = list(conf.loc[conf["Var"] == "nameObjTr", "Value"])[0]
-    name_obj_Co = list(conf.loc[conf["Var"] == "nameObjCo", "Value"])[0]
-    name_obj_clf = list(conf.loc[conf["Var"] == "nameObjCLF", "Value"])[0]
-    plotpathT = list(conf.loc[conf["Var"] == "DEplotpathT", "Value"])[0]
-    plotpathC = list(conf.loc[conf["Var"] == "DEplotpathC", "Value"])[0]
-
-    legRetListTr = tfg.load_obj(f"outputObj/legRetLists_{name_obj_Tr}")
-    DSA_TablesTr = tfg.load_obj(f"outputObj/DSA_Tables_{name_obj_Tr}")
-    DSA_GraphsTr = tfg.load_obj(f"outputObj/DSA_Graphs_{name_obj_Tr}")
-    DSA_MeanTr = tfg.load_obj(f"outputObj/DSA_mean_{name_obj_Tr}")
-    lst_Tr = (legRetListTr, DSA_TablesTr, DSA_GraphsTr, DSA_MeanTr)
-
-    legRetListCo = tfg.load_obj(f"outputObj/legRetLists_{name_obj_Co}")
-    DSA_TablesCo = tfg.load_obj(f"outputObj/DSA_Tables_{name_obj_Co}")
-    DSA_GraphsCo = tfg.load_obj(f"outputObj/DSA_Graphs_{name_obj_Co}")
-    DSA_MeanCo = tfg.load_obj(f"outputObj/DSA_mean_{name_obj_Co}")
-    lst_Co = (legRetListCo, DSA_TablesCo, DSA_GraphsCo, DSA_MeanCo)
-
-    up = tfg.load_obj(f"outputObj/up_{name_obj_clf}")
-    down = tfg.load_obj(f"outputObj/down_{name_obj_clf}")
-
-    objTr = r(f"objTr = readRDS('InputObj/{pathTret}')")
-    objCo = r(f"objCo = readRDS('InputObj/{pathControl}')")
-
-    # toVec = intersection(toVec, lst_Tr[0].keys())
-    # toVec = intersection(toVec, lst_Co[0].keys())
-    # projName = list(conf.loc[conf["Var"] == "Proj", "Value"])[0].split()
-
-    for toName in toVec:
-        fromVecTemp = utils.intersection(fromVec, lst_Tr[0][toName].keys())
-        fromVecTemp = utils.intersection(fromVecTemp, lst_Tr[0][toName].keys())
-        for fromName in fromVecTemp:
-            if fromName != toName:
-                if len(down[toName][fromName].index) > 0:
-                    deCirces(objCo, toName, fromName, lst_Co[0][toName][fromName],
-                             lst_Co[1][toName][fromName],
-                             lst_Co[2].return_GD_from_key(toName).return_GD_from_key(fromName),
-                             list(down[toName][fromName]["feture"]), plotpathC)
-
-                if len(up[toName][fromName].index) > 0:
-                    # executor.submit
-                    deCirces(objTr, toName, fromName, lst_Tr[0][toName][fromName],
-                             lst_Tr[1][toName][fromName],
-                             lst_Tr[2].return_GD_from_key(toName).return_GD_from_key(fromName),
-                             list(up[toName][fromName]["feture"]), plotpathT)
-
-
-def test_normaliztion():
-
-    conf = pd.read_csv("config.csv")
-
-    pathTret = list(conf.loc[conf["Var"] == "objTr", "Value"])[0]
-    pathControl = list(conf.loc[conf["Var"] == "objCo", "Value"])[0]
-
-    with localconverter(default_converter + rpyp.converter):
-        r("library(Seurat)")
-        readRDS = r("readRDS")
-        GetAssayData = r(f"function(obj) as.data.frame(obj[['RNA']]@{assay})")
-        GetObjIdent = r("function(obj) as.character(obj@active.ident)")
-        t1,t2 =  utils.normalize_scale_exp(pathTret,pathControl)
-
-
-
-def test_permutations():
-    conf = pd.read_csv("config.csv")
-    name_obj_Tr = list(conf.loc[conf["Var"] == "nameObjTr", "Value"])[0]
-    DSA_GraphsTr = tfg.load_obj(f"outputObj/DSA_Graphs_{name_obj_Tr}")
-    obj = DSA_GraphsTr.return_GD_from_key("1").return_GD_from_key("7").update_obj()
-    obj.perm_test_for_all_recptors()
-
-
 if __name__ == "__main__":
     print(os.getcwd())
-    #test_permutations()
     main()
-    #test_normaliztion()
